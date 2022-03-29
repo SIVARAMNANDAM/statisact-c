3703115048ce2ddb9d7222eccd0f3129e752c884
SIVARAMNANDAM <102355140+SIVARAMNANDAM@users.noreply.github.com>
2022-03-29 16:32:12 +0530

    uploaded Alert.c file

---
+++ alert.c
@@ -0,0 +1,19 @@
+#include "alert.h"
+#include "stats.h"
+void emailAlerter()
+{
+  emailAlertCallCount = 1;
+}
+void ledAlerter()
+{
+  ledAlertCallCount = 1;
+}
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+  
+  if(computedStats.max>maxThreshold)
+  {
+    emailAlerter();
+    ledAlerter();
+  }
+}
