3f1b8844b473cb2961ccb75830f989b5896339fa
SIVARAMNANDAM <102355140+SIVARAMNANDAM@users.noreply.github.com>
2022-03-30 18:19:14 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,13 +1,15 @@
+#ifndef STATS_H_
+#define STATS_H_
+
 struct Stats
 {
-  float average;
-  float min;
-  float max;
+	float average;
+	float max;
+	float min;
 };
-struct Stats compute_statistics(float* numberset, int setlength);
-float* compute_sort(float* numset, int length);
-typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
+#define NULL_VALUE 0
+
+struct Stats compute_statistics(const float* numberset, int setlength);
+ 
+#endif
