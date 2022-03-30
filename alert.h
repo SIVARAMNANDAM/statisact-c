d64aee88bba391d6e3fddb49ffab3548c4c23051
SIVARAMNANDAM <102355140+SIVARAMNANDAM@users.noreply.github.com>
2022-03-30 18:17:45 +0530

    Update alert.h

--- alert.h
+++ alert.h
@@ -1,2 +1,16 @@
+#ifndef ALERTER_H_
+#define ALERTER_H_
+
+#define NULL_PTR  (void*)0
+
+#define NO_OF_ALERTS 2
+
+typedef void (*alerter_funcptr[NO_OF_ALERTS])(void);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters, struct Stats computedStats);
 void emailAlerter(void);
 void ledAlerter(void);
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
+#endif /* ALERTER_H_ */
